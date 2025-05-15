cec60bf9f71e8666b21b0f9b41c36822b671b136
白彩恋 <shiro@oom-wg.dev>
2025-05-15 20:35:29 +0800

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -129,15 +129,12 @@ class NGAMsg {
       case NGAMsgType.err:
         icon = Icons.error_rounded;
         color = Colors.redAccent;
-        break;
       case NGAMsgType.warn:
         icon = Icons.warning_rounded;
         color = Colors.orangeAccent;
-        break;
       case NGAMsgType.ok:
         icon = Icons.check_circle_rounded;
         color = Colors.greenAccent;
-        break;
       case NGAMsgType.info:
         icon = Icons.info_rounded;
         color = Colors.blueAccent;
